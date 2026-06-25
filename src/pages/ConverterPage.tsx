366c2456e262e7ec88987957066f5c8ed06b3d91
GeryNi <gery.nicolay@spacebel.be>
2026-06-25 15:33:50 +0200

    WIP - draft converter page.

--- src/pages/ConverterPage.tsx
+++ src/pages/ConverterPage.tsx
@@ -6,25 +6,39 @@ export default function ConverterPage() {
   const { content } = useContext(JsonFileContext)!;
 
   return (
-    <div className="page-placeholder">
-        Coming soon.
-      {/*{content ? (*/}
-      {/*  <div className="upload-paste-area">*/}
-      {/*    <Editor*/}
-      {/*      height="650px"*/}
-      {/*      language="json"*/}
-      {/*      value={JSON.stringify(content, null, 2)}*/}
-      {/*      options={{*/}
-      {/*        readOnly: true,*/}
-      {/*        minimap: { enabled: false },*/}
-      {/*        scrollBeyondLastLine: false,*/}
-      {/*        tabSize: 2,*/}
-      {/*      }}*/}
-      {/*    />*/}
-      {/*  </div>*/}
-      {/*) : (*/}
-      {/*  <p>No JSON loaded yet. Upload or paste a file first.</p>*/}
-      {/*)}*/}
+    <>
+    <div className="upload-grid">
+      {content ? (
+        <div className="upload-paste-area">
+          <Editor
+            height="500px"
+            language="json"
+            // value={JSON.stringify(content, null, 2)}
+            options={{
+              readOnly: true,
+              minimap: { enabled: false },
+              scrollBeyondLastLine: false,
+              tabSize: 2,
+            }}
+          />
+        </div>
+      ) : (
+        <p>No JSON loaded yet. Upload or paste a file first.</p>
+      )}
     </div>
+    <div className="button-grid">
+        <button className="validate-btn"
+                disabled={true}>
+            Validate
+        </button>
+
+        {/*id results defined and valid button is not disabled di*/}
+        <button className="convert-btn"
+                disabled={true}
+                // disabled={!(result ? result.valid : false)}
+               >Download
+        </button>
+    </div>
+    </>
   );
 }
