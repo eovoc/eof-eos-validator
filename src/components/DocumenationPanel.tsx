ef28ca2920a34664f0958a5cbbf8519bdfa0ee35
GeryNi <gery.nicolay@spacebel.be>
2026-07-06 11:43:51 +0200

    Add more links for subsidiary schemas.

--- src/components/DocumenationPanel.tsx
+++ src/components/DocumenationPanel.tsx
@@ -4,6 +4,8 @@ export default function DocumentationPanel(){
     let links : DocumentationLink[] = [];
     links.push({title: "EO Metadata", href: `https://eof-eos.io.esa.int/eopf-eos/data-model/6-metadata.html`});
     links.push({title: "EOF-EOS Schema", href: `${process.env.PUBLIC_URL}/schemas/eof-eos-schema.json`});
+    links.push({title: "DQC Schema", href: `${process.env.PUBLIC_URL}/schemas/dqc.json`});
+    links.push({title: "MDJ Schema", href: `${process.env.PUBLIC_URL}/schemas/mdj.json`});
 
 
     const documentationLinks = links.map(link =>
