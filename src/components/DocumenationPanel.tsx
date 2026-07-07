1c4e9240f005611a569c63f4cf53c27c6b6d63fd
GeryNi <gery.nicolay@spacebel.be>
2026-07-07 16:44:03 +0200

    Documentation panel: support multiple links per element.

--- src/components/DocumenationPanel.tsx
+++ src/components/DocumenationPanel.tsx
@@ -1,26 +1,43 @@
 
 export default function DocumentationPanel(){
 
-    let links : DocumentationLink[] = [];
-    links.push({title: "EO Metadata", href: `https://eof-eos.io.esa.int/eopf-eos/data-model/6-metadata.html`});
-    links.push({title: "EOF-EOS Schema", href: `${process.env.PUBLIC_URL}/schemas/eof-eos-schema.json`});
-    links.push({title: "DQC Schema", href: `${process.env.PUBLIC_URL}/schemas/dqc.json`});
-    links.push({title: "MDJ Schema", href: `${process.env.PUBLIC_URL}/schemas/mdj.json`});
+    let resources : DocumentationResource[] = [];
+    //EO Metadata documentation
+    const metadataLink : Link = {title: "EO Metadata", href: `https://eof-eos.io.esa.int/eopf-eos/data-model/6-metadata.html`};
+    resources.push({links: [metadataLink]});
+
+    //EOF-EOS Schema documentation
+    const schemaLink = {title: "EOF-EOS Schema", href: `${process.env.PUBLIC_URL}/schemas/eof-eos-schema.json`};
+    resources.push({links: [schemaLink]});
+
+    //ISO19115-4 documentation
+    const dqcSchemaLink = {title: "DQC Schema", href: `${process.env.PUBLIC_URL}/schemas/dqc.json`};
+    const mdjSchemaLink = {title: "MDJ Schema", href: `${process.env.PUBLIC_URL}/schemas/mdj.json`};
+    resources.push({name: "ISO19115-4", links:[dqcSchemaLink,mdjSchemaLink]});
 
 
-    const documentationLinks = links.map(link =>
-        <li><a title={link.title} href={link.href} target="_blank" rel="noreferrer">{link.title}</a></li>
+
+    const documentationResources = resources.map(resource =>
+        <li>
+            {resource.name && <>{resource.name}: </>}
+            {resource.links.map(link =><a title={link.title} href={link.href} target="_blank" rel="noreferrer">{link.title}</a>)}
+        </li>
     );
 
     return(
         <>
             <h2>Documentation</h2>
-           <ul>{documentationLinks}</ul>
+           <ul>{documentationResources}</ul>
         </>
     );
 }
 
-export interface DocumentationLink {
+export interface DocumentationResource {
+    name?: string;
+    links: Link[];
+}
+
+export interface Link {
     title: string;
     href: string;
 }
