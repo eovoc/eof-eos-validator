47d3e97a6786cef45fcf43c5b8cc1410d0dd68f1
GeryNi <gery.nicolay@spacebel.be>
2026-07-09 10:17:49 +0200

    Documentation panel css changes for ISO19115-4 remove ':'.

--- src/components/DocumenationPanel.tsx
+++ src/components/DocumenationPanel.tsx
@@ -13,7 +13,7 @@ export default function DocumentationPanel(){
 
                 {/*ISO19115-4 documentation*/}
                 <li>
-                    ISO19115-4: (
+                    ISO19115-4 (
                         <a className="documentation-link" title="dqc.json" href={`${process.env.PUBLIC_URL}/schemas/dqc.json`} target="_blank" rel="noreferrer">dqc.json</a>
                     and
                     <a className="documentation-link" style={{ marginLeft: "0.25em" }} title="mdj.json" href={`${process.env.PUBLIC_URL}/schemas/mdj.json`} target="_blank" rel="noreferrer">mdj.json</a>
