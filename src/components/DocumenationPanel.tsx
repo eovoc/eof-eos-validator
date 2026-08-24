0130c8b792ccf285dc74ce60fe3d77d8bf7a2db3
GeryNi <gery.nicolay@spacebel.be>
2026-08-24 15:17:33 +0200

    Modification of Documentaion panel.

--- src/components/DocumenationPanel.tsx
+++ src/components/DocumenationPanel.tsx
@@ -8,10 +8,19 @@ export default function DocumentationPanel(){
                 {/*EO Metadata documentation*/}
                 <li> <a className="documentation-link" title="EO Metadata" href="https://eof-eos.io.esa.int/eopf-eos/data-model/6-metadata.html" target="_blank" rel="noreferrer">EO Metadata</a></li>
 
-                {/*EOF-EOS Schema documentation*/}
+                {/*EOF-EOS Schema Documentation*/}
+                <li> <a className="documentation-link" title="EOF-EOS Schema Documentation"
+                        href="https://json-schema.app/view/%23?url=https%3A%2F%2Feovoc.github.io%2Feof-eos-validator%2Fschemas%2Feof-eos-schema.json"
+                        target="_blank" rel="noreferrer">EOF-EOS Schema Documentation</a>
+                </li>
+            </ul>
+
+            <h2>Schemas</h2>
+            <ul>
+                {/*EOF-EOS Schema*/}
                 <li> <a className="documentation-link" title="EOF-EOS Schema" href={`${process.env.PUBLIC_URL}/schemas/eof-eos-schema.json`} target="_blank" rel="noreferrer">EOF-EOS Schema</a></li>
 
-                {/*ISO19115-4 documentation*/}
+                {/*ISO19115-4 Schemas*/}
                 <li>
                     ISO19115-4 (
                         <a className="documentation-link" title="dqc.json" href={`${process.env.PUBLIC_URL}/schemas/dqc.json`} target="_blank" rel="noreferrer">dqc.json</a>
